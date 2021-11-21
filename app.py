9594c375ca5019a211a33c90b9da4f3ad2e0fec3
Zignd <zignd.igor@gmail.com>
2021-11-21 14:53:43 -0300

    add endpoints to create accounts and retrieve them

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import time
+import uuid
 from flask import Flask, request, abort
 import logging
 
@@ -11,12 +12,7 @@ logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s [%(levelname)s] %(
                     ])
 
 db = {
-    'contas': [
-        {'id': 1, 'saldo': 150, 'is_locked': False, 'locked_by': None},
-        {'id': 2, 'saldo': 0, 'is_locked': False, 'locked_by': None},
-        {'id': 3, 'saldo': 4800000, 'is_locked': False, 'locked_by': None},
-        {'id': 4, 'saldo': 100, 'is_locked': True, 'locked_by': 3},
-    ]
+    'contas': []
 }
 
 tokens = [
@@ -58,6 +54,11 @@ def raise_locked(responsavel):
         'codigo_erro': COD_ERR_CONTA_COM_LOCK,
         'responsavel': responsavel,
     }, 423)))
+
+
+@app.get('/')
+def get_home():
+    return 'API do servidor de dados'
 
 
 @app.get('/conta/<conta_id>/saldo')
@@ -103,8 +104,6 @@ def set_saldo(conta_id, valor):
     if conta['is_locked']:
         raise_locked(conta['locked_by'])
 
-    valor = int(valor)
-
     conta['is_locked'] = True
     conta['locked_by'] = serv_negocio_id
 
@@ -117,7 +116,7 @@ def set_saldo(conta_id, valor):
     conta['is_locked'] = False
     conta['locked_by'] = None
 
-    conta['saldo'] = valor
+    conta['saldo'] = int(valor)
 
     app.logger.debug('Saldo da conta atualizado {}'.format(
         {'conta': conta['id'], 'saldo': conta['saldo']}))
@@ -125,4 +124,66 @@ def set_saldo(conta_id, valor):
     return {
         'conta': conta['id'],
         'saldo': conta['saldo'],
+    }, 200
+
+
+@app.post('/conta')
+def post_conta():
+    body = request.get_json()
+    saldo_inicial_desejado = body['saldo']
+    if len(db['contas']) == 0:
+        id = 1
+    else:
+        ultimo_id = max(db['contas'], key=lambda conta: conta['id'])['id']
+        id = ultimo_id + 1
+    nova_conta = {
+        'id': id,
+        'saldo': saldo_inicial_desejado,
+        'is_locked': False,
+        'locked_by': None,
+        'token': str(uuid.uuid4())
+    }
+    db['contas'].append(nova_conta)
+    return {
+        'conta': nova_conta['id'],
+        'saldo': nova_conta['saldo'],
+        'is_locked': nova_conta['is_locked'],
+        'locked_by': nova_conta['locked_by'],
+        'token': nova_conta['token']
+    }, 200
+
+
+@app.get('/conta/<conta_id>')
+def get_conta(conta_id):
+    conta = next(
+        (
+            conta
+            for conta in db['contas']
+            if conta['id'] == int(conta_id)
+        ),
+        None
+    )
+    if conta == None:
+        raise_conta_inexistente()
+    if conta['is_locked']:
+        raise_locked(conta['locked_by'])
+    return {
+        'conta': conta['id'],
+        'saldo': conta['saldo'],
+        'is_locked': conta['is_locked'],
+        'locked_by': conta['locked_by'],
+        'token': conta['token']
+    }, 200
+
+
+@app.get('/conta')
+def get_contas():
+    return {
+        'contas': [{
+            'conta': conta['id'],
+            'saldo': conta['saldo'],
+            'is_locked': conta['is_locked'],
+            'locked_by': conta['locked_by'],
+            'token': conta['token']
+        } for conta in db['contas']]
     }, 200
